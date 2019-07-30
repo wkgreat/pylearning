13b9dcadd23e6695b17a43c223179be07651aa31
wkgreat <wkgreat@outlook.com>
2019-07-30 20:09:43 +0800

    practice of linear regression

---
+++ practice/linear_regression.py
@@ -0,0 +1,48 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+
+def build_func(a=2):
+    def the_func(x):
+        return a * x
+
+    return the_func
+
+def main():
+
+    target = 10
+
+    def lossFunc(a, x, y):
+        return abs(y.T - x.T * a)
+
+    def deltaFunc(a, x, y):
+        alpha = 0.0001
+        return -1 * 2 * alpha * ((a * x - y) * x.T) / 1000
+
+    x = np.linspace(-100, 100, 1000)
+    y = np.array([build_func(target)(xx)+random.random()*1000 for xx in x])
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    sp, = ax.plot(x, y)
+
+    a = 0.1
+    mx = np.mat(x)
+    my = np.mat(y)
+
+    for i in range(20):
+        delta = deltaFunc(a, mx, my)
+        loss = lossFunc(a, mx, my)
+        mean_loss = np.sum(loss) * 1.0 / loss.shape[0]
+        a += delta[0, 0]
+        print a
+
+        ax.plot(x, np.array([build_func(a)(xx) for xx in x]))
+
+    plt.ioff()
+    plt.show()
+
+    print a
+
+if __name__ == '__main__':
+    main()
